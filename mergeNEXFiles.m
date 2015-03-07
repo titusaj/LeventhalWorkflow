50bc4f65607d4bc47263fb9bb987391a18faff3e
Matt Gaidica <matt@gaidi.ca>
2015-03-07 01:11:41 -0500

    Workflow complete, writing mat files too

--- mergeNEXFiles.m
+++ mergeNEXFiles.m
@@ -4,4 +4,5 @@ leventhalPaths = buildLeventhalPaths(nasPath,sessionName);
 disp('Making box.nex file...');
 TDTtoNex(nasPath,sessionName);
 disp('Merging all .nex files...');
-combineSessionNex_wf(leventhalPaths.processed);
+parts = strsplit(leventhalPaths.processed,filesep);
+combineSessionNex_wf(fullfile(parts{1:end-1}));
