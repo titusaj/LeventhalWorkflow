496bf20810818fde393898cb5deeccee04e1bd66
Matt Gaidica <matt@gaidi.ca>
2015-03-08 12:08:24 -0400

    Put PLX functions in createPLXFiles

--- createPLXFiles.m
+++ createPLXFiles.m
@@ -63,3 +63,62 @@ function ch = getSEVChFromFilename(name)
     C = strsplit(C{end},'.'); %C{1} = chXX
     ch = str2double(C{1}(3:end));
 end
+
+function makePLXChannelHeader(PLXid,sessionConf,tetrodeChannels,tetrodeName)
+    for ii=1:length(tetrodeChannels)
+        chInfo.tetName  = [sessionConf.sessionName,'_',tetrodeName];
+        chInfo.wireName = sprintf('%s_W%02d', tetrodeName, ii);
+
+        chInfo.wireNum   = ii; %tetrode number
+        chInfo.WFRate    = sessionConf.Fs;
+        chInfo.SIG       = tetrodeChannels(ii);  %channel number
+        chInfo.refWire   = 0;     % not sure exactly what this is; Alex had it set to zero
+        chInfo.gain      = 300;
+        chInfo.filter    = 0;    % not sure what this is; Alex had it set to zero
+        chInfo.thresh    = 0; % does this even matter anywhere?
+        chInfo.numUnits  = 0;    % no sorted units
+        chInfo.sortWidth = sessionConf.waveLength;
+        chInfo.comment   = 'created with Spikey, makePLXChannelHeader';
+
+        writePLXChanHeader(PLXid, chInfo);
+    end
+end
+
+function PLXid = makePLXInfo(PLXfn,sessionConf,tetrodeChannels,dataLength)
+    sessionDateStr = sessionConf.sessionName(7:14);
+    sessionDateVec = datevec(sessionDateStr, 'yyyymmdd');
+
+    plxInfo.comment    = '';
+    plxInfo.ADFs       = sessionConf.Fs; % record the upsampled Fs as the AD freq for timestamps
+    plxInfo.numWires   = length(tetrodeChannels);
+    plxInfo.numEvents  = 0;
+    plxInfo.numSlows   = 0;
+    plxInfo.waveLength = sessionConf.waveLength;
+    plxInfo.peakLoc    = sessionConf.peakLoc;
+
+    plxInfo.year  = sessionDateVec(1);
+    plxInfo.month = sessionDateVec(2);
+    plxInfo.day   = sessionDateVec(3);
+
+    timeVector = datevec('12:00','HH:MM');
+    plxInfo.hour       = timeVector(4);
+    plxInfo.minute     = timeVector(5);
+    plxInfo.second     = 0;
+    plxInfo.waveFs     = sessionConf.Fs; % record the upsampled Fs as the waveform sampling frequency
+    plxInfo.dataLength = dataLength;
+
+    plxInfo.Trodalness     = length(tetrodeChannels); %Trodalness - 0,1 = single electrode, 2 = stereotrode, 4 = tetrode
+    plxInfo.dataTrodalness = 0; %this is set to 0 in the Plexon tetrode sample file
+
+    plxInfo.bitsPerSpikeSample = 16;
+    plxInfo.bitsPerSlowSample  = 16;
+
+    plxInfo.SpikeMaxMagnitudeMV = 10000;
+    plxInfo.SlowMaxMagnitudeMV  = 10000;
+
+    plxInfo.SpikePreAmpGain = 1; % gain before final amplification stage
+
+    PLXid = fopen(PLXfn, 'w');
+    disp('PLX file opened...')
+    writePLXheader(PLXid, plxInfo);
+end
